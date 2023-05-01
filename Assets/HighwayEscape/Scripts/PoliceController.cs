0b0bc59b01af1f6b171c291110fc0f0db639c4ac
Diego <diegopartidaromero@gmail.com>
2023-05-01 01:26:11 -0600

    add police

--- Assets/HighwayEscape/Scripts/PoliceController.cs
+++ Assets/HighwayEscape/Scripts/PoliceController.cs
@@ -1,24 +1,177 @@
 using UnityEngine;
+using System.Collections;
+using SgLib;
 
 public class PoliceController : MonoBehaviour
 {
-    public GameObject player;  // Referencia al GameObject del jugador
-    public float speed = 5f;   // Velocidad del policía
 
-    private Vector3 targetPos; // Posición objetivo del policía
 
-    void Start()
+public float currentSpeed;
+public float maxAngularVelocity = 15;
+
+private Rigidbody rigid;
+private bool finishTurn;
+
+
+void Start()
+{
+
+
+
+    finishTurn = true;
+    //currentSpeed = GameManager.Instance.initialSpeed;
+    currentSpeed = 20;
+    rigid = GetComponent<Rigidbody>();
+
+
+    rigid.maxAngularVelocity = maxAngularVelocity;
+    rigid.ResetCenterOfMass();
+}
+
+void FixedUpdate()
+{
+    //Move player and particle
+    if (GameManager.Instance.GameState == GameState.Playing)
     {
-        // La posición inicial del policía será detrás del jugador
-        targetPos = player.transform.position - player.transform.forward * 5f;
+        rigid.velocity = Vector3.forward * currentSpeed;
     }
+}
 
+    // Update is called once per frame
     void Update()
+{
+    if (GameManager.Instance.GameState.Equals(GameState.Playing))
     {
-        // Mover el policía hacia la posición objetivo del jugador
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        if (finishTurn)
+        {
+            // Movimiento hacia la derecha
+            if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+            {
+                StartCoroutine(TurnRight());
+                StartCoroutine(Rotate(GameManager.Instance.rotateAngle));
+   
+            }
 
-        // Actualizar la posición objetivo del policía para que siempre esté detrás del jugador
-        targetPos = player.transform.position - player.transform.forward * 5f;
+            // Movimiento hacia la izquierda
+            if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+            {
+                StartCoroutine(TurnLeft());
+                StartCoroutine(Rotate(-GameManager.Instance.rotateAngle));;
+            }
+
+            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+            {
+                currentSpeed += GameManager.Instance.increaseSpeedFactor * Time.deltaTime;
+                if (currentSpeed > 95)
+                {
+                    currentSpeed = 95;
+                }
+            }
+
+            
+            if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W))
+            {
+                if (currentSpeed > GameManager.Instance.playerSpeed)
+                {
+                    currentSpeed -= GameManager.Instance.increaseSpeedFactor * Time.deltaTime / 10;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+            {
+                currentSpeed -= GameManager.Instance.increaseSpeedFactor * Time.deltaTime;
+                if (currentSpeed < 20)
+                {
+                    currentSpeed = 20;
+                }
+            }
+        }
     }
+}
+    IEnumerator TurnRight()
+    {
+        finishTurn = false;
+
+        yield return new WaitForFixedUpdate();
+
+        float startX = Mathf.Round(transform.position.x);
+        float endX = startX + 4f;
+
+        if (endX <= 8)
+        {
+            float t = 0;
+            while (t < GameManager.Instance.turnTime)
+            {
+                t += Time.deltaTime;
+                float fraction = t / GameManager.Instance.turnTime;
+                float newX = Mathf.Lerp(startX, endX, fraction);
+                Vector3 newPos = transform.position;
+                newPos.x = newX;
+                transform.position = newPos;
+                yield return null;
+            }
+        }      
+        finishTurn = true;
+    }
+
+    IEnumerator TurnLeft()
+    {
+        finishTurn = false;
+
+        yield return new WaitForFixedUpdate();
+
+        float startX = Mathf.Round(transform.position.x);
+        float endX = startX - 4f;
+
+        if (endX >= -8)
+        {
+            float t = 0;
+            while (t < GameManager.Instance.turnTime)
+            {
+                t += Time.deltaTime;
+                float fraction = t / GameManager.Instance.turnTime;
+                float newX = Mathf.Lerp(startX, endX, fraction);
+                Vector3 newPos = transform.position;
+                newPos.x = newX;
+                transform.position = newPos;
+                yield return null;
+            }
+        }
+
+        finishTurn = true;
+    }
+
+    IEnumerator Rotate(float angle)
+    {
+        finishTurn = false;
+
+        yield return new WaitForFixedUpdate();
+
+        if (transform.position.x < 8 && transform.position.x > -8)
+        {
+            Quaternion startRot = transform.rotation;
+            Quaternion endRot = Quaternion.Euler(0, angle, 0);
+            float t = 0;
+            while (t < GameManager.Instance.turnTime / 2f)
+            {
+                t += Time.deltaTime;
+                float fraction = t / (GameManager.Instance.turnTime / 2f);
+                transform.rotation = Quaternion.Lerp(startRot, endRot, fraction);
+                yield return null;
+               
+            }
+
+            float r = 0;
+            while (r < GameManager.Instance.turnTime / 2f)
+            {
+                r += Time.deltaTime;
+                float fraction = r / (GameManager.Instance.turnTime / 2f);
+                transform.rotation = Quaternion.Lerp(endRot, startRot, fraction);
+                yield return null;
+            }
+
+        }
+        finishTurn = true;
+    }
+
 }
